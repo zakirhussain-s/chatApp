ad7227db3b5b8b6f1f6fcd154c1bc41d7c2e4222
Zakir Hussain S <s.zakirhussain02@gmail.com>
2025-02-11 23:52:57 +0530

    Update WebSocketConfig.java

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
-/**
- * Created by rajeevkumarsingh on 24/07/17.
- */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wss").withSockJS();
+        // Adding setAllowedOrigins("*") to allow connections in production
+        registry.addEndpoint("/wss")
+               .setAllowedOrigins("*")
+               .withSockJS();
     }
 
     @Override
